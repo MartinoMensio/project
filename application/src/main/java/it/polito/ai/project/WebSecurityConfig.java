9315e0f72e2dd9df285fa0ad92b63abb6920f01d
Martino Mensio <martinomensio@outlook.it>
2017-06-11 16:29:07 +0200

    copied assignment5 angular app

--- application/src/main/java/it/polito/ai/project/WebSecurityConfig.java
+++ application/src/main/java/it/polito/ai/project/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// definition of the request filtering
 		.authorizeRequests()
 			// the permitted URLs without authentication
-			.antMatchers("/", "/home", "/login", "/registration", "/about", "/contact", "/registrationResult","/css/**", "/webjars/**","/rest/**").permitAll()
+			.antMatchers("/app/**", "/", "/home", "/login", "/registration", "/about", "/contact", "/registrationResult","/css/**", "/webjars/**","/rest/**").permitAll()
 			// all the other URLs require authentication
 			.antMatchers("/**").hasRole("USER")
 		.and()
