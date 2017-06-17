5dad1496763a4e03fbddb343f665aad76930bf10
Alessio Ciavarella <ale.ciav92@gmail.com>
2017-06-17 19:46:35 +0200

    Definition of directive message for the chat

---
+++ frontend/directives/chatMessage.js
@@ -0,0 +1,27 @@
+var app = angular.module('App');
+
+app.directive('message', function() {
+    return {
+        scope: {
+            username: '@',
+            timestamp: '@',
+            text: '@',
+            side: '@'
+        },
+        link: function link(scope, element, attrs) {
+            if (scope.side == "left") {
+                scope.li_pos = "left";
+                scope.pull = "pull-left";
+                scope.username_pos = "";
+                scope.timestamp_pos = "pull-right";
+            }
+            else if (scope.side == "right") {
+                scope.li_pos = "right";
+                scope.pull = "pull-right";
+                scope.username_pos = "pull-right";
+                scope.timestamp_pos = "align-right";
+            }
+        },
+        templateUrl: "../templates/chatMessage.html"
+    };
+});
