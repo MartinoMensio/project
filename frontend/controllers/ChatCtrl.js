d4e7311f07ef76bef4d27dd34dd8ffd45fb61393
Alessio Ciavarella <ale.ciav92@gmail.com>
2017-06-18 18:46:19 +0200

    Modal definition for the new warning form

--- frontend/controllers/ChatCtrl.js
+++ frontend/controllers/ChatCtrl.js
@@ -1,6 +1,6 @@
 var app = angular.module('App');
 
-app.controller('ChatCtrl', ['$scope', function ($scope) {
+app.controller('ChatCtrl', ['$scope', '$uibModal', function ($scope, $uibModal) {
     this.messages = [];
     this.msg_text = "";
 
@@ -41,5 +41,30 @@ app.controller('ChatCtrl', ['$scope', function ($scope) {
         }
     }
 
+
+    this.openNewWarningModal = function (size, parentSelector) {
+        //var parentElem = parentSelector ? angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+        
+        var modalInstance = $uibModal.open({
+            /*ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',*/
+            templateUrl: 'templates/modals/newWarningModal.html',
+            /*controller: 'ModalInstanceCtrl',
+            controllerAs: '$ctrl',*/
+            size: 'lg'
+            /*appendTo: parentElem,*/
+            /*resolve: {
+                items: function () {
+                return $ctrl.items;
+                }
+            }*/
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+            $ctrl.selected = selectedItem;
+            }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
 
 }]);
