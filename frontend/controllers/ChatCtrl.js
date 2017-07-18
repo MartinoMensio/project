5e23d607f1e22f070c7c61d192253e0494b8d9da
Riccardo Persiani <s225289@studenti.polito.it>
2017-07-18 16:06:13 +0200

    Show alert icons in every chat map

--- frontend/controllers/ChatCtrl.js
+++ frontend/controllers/ChatCtrl.js
@@ -3,6 +3,16 @@ var app = angular.module('App');
 app.controller('ChatCtrl', ['$scope', '$uibModal', '$stateParams', '$localStorage', 'ChatService', 'AlertsService', function ($scope, $uibModal, $stateParams, $localStorage, ChatService, AlertsService) {
     this.template = "templates/popovers/popoverTemplate.html";
 
+    var local_icons = {
+        default_icon: {},
+        leaf_icon: {
+            iconUrl: 'icons/alert1.png',
+            iconSize:     [50, 50], // size of the icon
+            iconAnchor:   [30, 30], // point of the icon which will correspond to marker's location
+            popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+        },
+    };
+
     // define the map status: shown/not shown
     this.showMap = false;
 
@@ -71,6 +81,31 @@ app.controller('ChatCtrl', ['$scope', '$uibModal', '$stateParams', '$localStorag
 
     this.mapToggle = () => {
         this.showMap = !this.showMap;
+        // get the list of all teh alerts
+        AlertsService.getAlerts().then((result) => {
+            this.alerts = result; // show the alert list
+
+            this.hashtag = result.hashtag;
+            this.markers = result; // show the markers on the map
+
+            // each marker display the 5 star for rating the alert
+            this.markers.forEach((marker,index) => {
+                marker.getMessageScope = ()=> { return $scope; }
+                var initialization = true;
+                // watch added to check if a new rate is inserted by the user
+                $scope.$watch("ctrl.alerts["+index+"].newRating", (newValue, oldValue)=> {
+                    if (initialization === true || newValue===0) {
+                        initialization = false;
+                        return;
+                    }
+                    this.vote(marker);
+                });
+                // 5 starts used by the user for rating the signalization and 5 stars readonly for the rating avg
+                //marker.iconUrl="icons/alert.png";
+                marker.icon= local_icons.leaf_icon,
+                marker.message = '<div style="min-width:160px;"></div><h2>#'+marker.hashtag+'</h2> <h5>Vote Here</h5><input-stars ng-model="ctrl.markers['+index+'].newRating" max="5"></input-stars> </br><h5>Average</h5> <input-stars max="5" ng-model="ctrl.markers['+index+'].rating" readonly="true" allow-half ></input-stars>';
+            }, this);
+        });
     }
 
     // this function is called every time the message text changes 
