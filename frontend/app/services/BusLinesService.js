7b81bee4234106a0e75aaec02f0740472a3e1963
Martino Mensio <martinomensio@outlook.it>
2017-07-22 16:40:25 +0200

    removing DataProvider.js and linee.js
    all the informations are retrieved using GeoServices
    closes #14

--- frontend/app/services/BusLinesService.js
+++ frontend/app/services/BusLinesService.js
@@ -1,6 +1,6 @@
 var app = angular.module('App');
 
-app.factory('BusLinesService', ['$http', '$q', 'DataProvider', function ($http, $q, DataProvider) {
+app.factory('BusLinesService', ['$http', '$q', function ($http, $q) {
 
     var endpoint = 'http://localhost:9999/';
 
