687ce37700e9ae84fe037b13c5e23e0ab09296ed
Martino Mensio <martinomensio@outlook.it>
2017-07-22 10:28:08 +0200

    enable cross origin for GeoServices repository

--- GeoServices/src/main/java/it/polito/ai/project/postgis/repo/BusLinesRepository.java
+++ GeoServices/src/main/java/it/polito/ai/project/postgis/repo/BusLinesRepository.java
@@ -7,11 +7,13 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import it.polito.ai.project.postgis.entities.BusLine;
 
 @Repository
 @RepositoryRestResource
+@CrossOrigin(origins = "*")
 public interface BusLinesRepository extends PagingAndSortingRepository<BusLine, String> {
 
 	@RestResource(path="getByLine")
--- GeoServices/src/main/java/it/polito/ai/project/postgis/repo/BusStopsGeoRepository.java
+++ GeoServices/src/main/java/it/polito/ai/project/postgis/repo/BusStopsGeoRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import it.polito.ai.project.postgis.entities.BusStopGeographic;
 
 @Repository
+@CrossOrigin(origins = "*")
 public interface BusStopsGeoRepository extends PagingAndSortingRepository<BusStopGeographic, String> {
 	
 	/**
