39e21e9049b08844d41f949b8585c3c570c5ae08
Martino Mensio <martinomensio@outlook.it>
2017-07-22 08:57:32 +0200

    exposing id for BusLine and BusStop and adding REST resource to search lines by partial name

--- GeoServices/src/main/java/it/polito/ai/project/postgis/repo/BusLinesRepository.java
+++ GeoServices/src/main/java/it/polito/ai/project/postgis/repo/BusLinesRepository.java
@@ -1,13 +1,19 @@
 package it.polito.ai.project.postgis.repo;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import it.polito.ai.project.postgis.entities.BusLine;
 
 @Repository
 @RepositoryRestResource
-public interface BusLinesRepository  extends PagingAndSortingRepository<BusLine, String> {
+public interface BusLinesRepository extends PagingAndSortingRepository<BusLine, String> {
 
+	@RestResource(path="getByLine")
+	public List<BusLine> getByLineContainingIgnoreCase(@Param("line") String line);
 }
