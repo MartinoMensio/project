725b2a4dae2cc3c37a495439c54e464004759cc3
Martino Mensio <martinomensio@outlook.it>
2017-07-23 22:59:31 +0200

    merge fix

--- frontend/server.js
+++ frontend/server.js
@@ -1,9 +1,9 @@
-    port: process.env.PORT || 8080,
 var fs = require('fs');
 var path = require('path');
 var liveServer = require('live-server');
 
 liveServer.start({
+    port: process.env.PORT || 8080,
     open: false,
-    root: path.resolve(__dirname, 'app/'),
+    root: path.resolve(__dirname, 'app/')
 });
